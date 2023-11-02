badbfc39a69d02e0e52c49b1cec63b116e0ce3a3
Joseph Bak <joseph.bak31415@gmail.com>
2023-11-02 18:43:27 -0400

    docstrings

--- lecture9/meows.py
+++ lecture9/meows.py
@@ -1,6 +1,6 @@
 # Constants
 # Type Hints, mypy
-
+# Docstrings
 
 # class Cat:
 #     MEOWS = 3
@@ -9,11 +9,19 @@
 #         for _ in range(Cat.MEOWS):
 #             print("meow")
 
-
 # cat = Cat()
 # cat.meow()
 
 def meow(n: int) -> str:
+    """
+    Meow n times.
+
+    :param n: Number of times to meow
+    :type n: int
+    :raise TypeError: If n is not an int
+    :return: A string of n meows, one per line
+    :rtype: str
+    """
     return "meow\n" * n
     # for _ in range(n):
     #     print("meow")
