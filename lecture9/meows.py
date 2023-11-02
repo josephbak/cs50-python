d91b2a49e23b1697075265f2ac83346a89bcbcd0
Joseph Bak <joseph.bak31415@gmail.com>
2023-11-02 18:33:53 -0400

    type hints, mypy

--- lecture9/meows.py
+++ lecture9/meows.py
@@ -1,12 +1,24 @@
 # Constants
-
-class Cat:
-    MEOWS = 3
-
-    def meow(self):
-        for _ in range(Cat.MEOWS):
-            print("meow")
+# Type Hints, mypy
 
 
-cat = Cat()
-cat.meow()
+# class Cat:
+#     MEOWS = 3
+
+#     def meow(self):
+#         for _ in range(Cat.MEOWS):
+#             print("meow")
+
+
+# cat = Cat()
+# cat.meow()
+
+def meow(n: int) -> str:
+    return "meow\n" * n
+    # for _ in range(n):
+    #     print("meow")
+
+
+number: int = int(input("Number: "))
+meows: str = meow(number)
+print(meows, end="")
