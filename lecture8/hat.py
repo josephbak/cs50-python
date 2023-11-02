b57156b1be459f3f8da8480db5142c4aed4a9f81
Joseph Bak <joseph.bak31415@gmail.com>
2023-11-02 11:04:01 -0400

    classmethod practicel

--- lecture8/hat.py
+++ lecture8/hat.py
@@ -1,4 +1,14 @@
-class Hat:
-    ...
+import random
 
-hat = Hat()
+
+class Hat:
+
+    # clss variable
+    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
+    @classmethod
+    def sort(cls, name):
+        print(name, "is in", random.choice(cls.houses))
+
+
+Hat.sort("Harry")
