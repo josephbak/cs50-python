99834bb862292d95ae7552f14cdefd83ebc3bbaa
Joseph Bak <joseph.bak31415@gmail.com>
2023-11-02 11:34:17 -0400

    inheritance

--- lecture8/wizard.py
+++ lecture8/wizard.py
@@ -1,7 +1,7 @@
 class Wizard:
     def __init__(self, name):
         if not name:
-            raise ValueError("Missin name")
+            raise ValueError("Missing name")
         self.name = name
 
 class Student(Wizard):
@@ -13,3 +13,11 @@ class Professor(Wizard):
     def __init__(self, name, subject):
         super().__init__(name)
         self.subject = subject
+
+
+wizard = Wizard("Albus")
+student = Student("Harry", "Gryffindor")
+professor = Professor("Severus", "Defense Against the Dark Arts")
+
+# if __name__ == "__main__":
+#     main
