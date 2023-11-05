259a8018c9fbbcb10b8847bac8652f1e257cb081
Joseph Bak <joseph.bak31415@gmail.com>
2023-11-05 12:35:12 -0500

    lec5 calculator test updated

--- lecture5_unit_tests/test_calculator.py
+++ lecture5_unit_tests/test_calculator.py
@@ -7,12 +7,8 @@ def main():
     test_square()
 
 def test_square():
-    # if square(2) != 4:
-    #     print("2 squared was not 4")
-    # if square(3) != 9:
-    #     print("3 squared was not 9")
     try:
-        assert square(2) == 4
+        assert square(2) == 5
     except AssertionError:
         print("2 squared was not 4")
     try:
@@ -26,11 +22,15 @@ def test_square():
     try:
         assert square(-3) == 9
     except AssertionError:
-        print("-3 squared was not 9")
+        print("-3 squared was not 5")
     try:
         assert square(0) == 0
     except AssertionError:
         print("0 squared was not 0")
+    # if square(2) != 4:
+    #     print("2 squared was not 4")
+    # if square(3) != 9:
+    #     print("3 squared was not 9")
 
 
 if __name__ == "__main__":
