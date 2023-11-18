79a9a925a33a9f885351b8dc5a3a344ee89b90fd
Joseph Bak <joseph.bak31415@gmail.com>
2023-11-18 14:48:33 -0500

    cs50p problem set 7 working done

--- problems/problem7/working/working.py
+++ problems/problem7/working/working.py
@@ -1,54 +1,72 @@
 import re
-import sys
-
 
 def main():
-    # print(convert(input("Hours: ")))
-    convert(input("Hours: "))
+    print(convert(input("Hours: ")))
 
 
 def convert(s):
-    time_range_pattern = re.compile(r'\b([1-9]|1[0-2])\s?(?::[0-5][0-9])?\s?(AM|PM|am|pm)?\s?to\s?([1-9]|1[0-2])\s?(?::[0-5][0-9])?\s?(AM|PM|am|pm)?\b')
-    # time_range_pattern = re.compile(r'\b(\d{1,2}(?::\d{2})?)\s?(AM|PM|am|pm)?\s?to\s?(\d{1,2}(?::\d{2})?)\s?(AM|PM|am|pm)?\b')
-    # time_range_pattern = re.compile(r'\b(\d{1,2}(?::[0-5][0-9])?)\s?(AM|PM|am|pm)?\s?to\s?(\d{1,2}(?::[0-5][0-9])?)\s?(AM|PM|am|pm)?\b')
-    # time_range_pattern = re.compile(r'\b(\d{1,2}(?::[0-5][0-9])?)\s?(AM|PM|am|pm)?\s?to\s?(\d{1,2}(?::[0-5][0-9])?)\s?(AM|PM|am|pm)?\b')
+    time_pattern = re.compile(r'\b(?:\d{1,2}(:[0-5]\d)?\s*[APMapm]+ to \d{1,2}(:[0-5]\d)?\s*[APMapm]+)\b')
 
     # Test the regular expression
-    test_strings = [
-        "9AM to 5PM",
-        "9:00 AM to 5:00 PM",
-        "10 PM to 8 AM",
-        "10:30 PM to 8:50 AM",
-        "9:60 AM to 5:60PM",
-        "9 AM - 5 PM",
-        "09:00 AM - 17:00PM"
-    ]
+    # test_strings = [
+    #     "9 AM to 5 PM",
+    #     "9:00 AM to 5:00 PM",
+    #     "10 PM to 8 AM",
+    #     "10:30 PM to 8:50 AM",
+    #     "9:60 AM to 5:60PM",
+    #     "9 AM - 5 PM",
+    #     "09:00 AM - 17:00PM"
+    # ]
 
-    for test_string in test_strings:
-        match = time_range_pattern.search(test_string)
-        if match:
-
-            print(f"Match: {test_string}")
-            print(f"Start Hour: {match.group(1)}")
-            print(f"Start AM/PM: {match.group(2)}" if match.group(2) else "")
-            print(f"End Hour: {match.group(3)}")
-            print(f"End AM/PM: {match.group(4)}" if match.group(4) else "")
-            print()
-
-            # if match.group(2).lower() == "am":
-
-
-        else:
-            print(f"No match: {test_string}")
-    # print(s)
-
-    # match = time_range_pattern.search(s)
+    match = time_pattern.search(s)
     # print(match)
-    # if match:
-    #     print(match.group(0))
-    # else:
-    #     print("none")
+    if match:
+        s_list = s.split(" ") # 9:00, AM, to, 5, PM
+        # print(s_list)
+        if s_list[1].lower() == "pm":
+            if s_list[0].split(":")[0] == "12":
+                s_list[0] = "12:00"
+            else:
+                if ":" in s_list[0]: # ex) 9:00
+                    s_list[0] = str(int(s_list[0].split(":")[0]) + 12) + ":" + s_list[0].split(":")[1]
+                else: # ex) 9
+                        s_list[0] = str(int(s_list[0]) + 12) + ":00"
+        else:
+            if int(s_list[0].split(":")[0]) < 10:
+                if ":" in s_list[0]: # ex) 9:00
+                    s_list[0] = "0" + s_list[0]
+                else: # ex) 9
+                    s_list[0] = "0" + s_list[0] + ":00" 
+            elif int(s_list[0].split(":")[0]) == 12:
+                s_list[0] = "00:00"
+            else:
+                if ":" not in s_list[0]: # ex) 9
+                    s_list[0] = s_list[0] + ":00"
 
+        if s_list[4].lower() == "pm":
+            if s_list[3].split(":")[0] == "12":
+                s_list[3] = "12:00"
+            else:
+                if ":" in s_list[3]: # ex) 9:00
+                    s_list[3] = str(int(s_list[3].split(":")[0]) + 12) + ":" + s_list[3].split(":")[1]
+                else: # ex) 9
+                    s_list[3] = str(int(s_list[3]) + 12) + ":00"
+        else:
+            if int(s_list[3].split(":")[0]) < 10:
+                if ":" in s_list[3]: # ex) 9:00
+                    s_list[3] = "0" + s_list[3]
+                else: # ex) 9
+                    s_list[3] = "0" + s_list[3] + ":00" 
+            elif int(s_list[3].split(":")[0]) == "12":
+                s_list[3] = "00:00"
+            else:
+                if ":" not in s_list[0]: # ex) 9
+                    s_list[0] = s_list[0] + ":00"
+
+        return s_list[0] + " to " + s_list[3]
+
+    else:
+        raise ValueError("value error")
 
 if __name__ == "__main__":
     main()
