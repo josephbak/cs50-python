2985d788e8981f1b8c37fdd68f06ff17157503ac
Joseph Bak <joseph.bak31415@gmail.com>
2023-11-17 19:09:32 -0500

    problem set 7 numb3rs done

--- problems/problem7/numb3rs/numb3rs.py
+++ problems/problem7/numb3rs/numb3rs.py
@@ -1,16 +1,24 @@
 import re
-import sys
-
 
 def main():
     print(validate(input("IPv4 Address: ")))
+    # validate(input("IPv4 Address: "))
 
 
 def validate(ip):
-    ...
-
-
-...
+    if len(ip.split(".")) != 4:
+        return False
+    else:
+        match = re.search(r"(\d+)\.(\d+)\.(\d+)\.(\d+)", ip)
+        print(match.group())
+        ip_list = []
+        if match:
+            for i in range(1, 5):
+                ip_list.append(int(match.group(i)))
+            print(ip_list)
+            return all(ele >= 0 for ele in ip_list) and all(ele <= 255 for ele in ip_list)
+        else:
+            return False
 
 
 if __name__ == "__main__":
